60c4ceb9f3a89138dcc866eacd910276820bf184
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-03-15 15:19:17 +0200

    constants

---
+++ lib/constants/images.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBearish 1
+  /// assets/images/bearish 1.png
+  static const String imagesBearish  = "assets/images/bearish 1.png";
+
+  /// Assets for imagesOnbooardingimage
+  /// assets/images/onbooardingimage.png
+  static const String imagesOnbooardingimage = "assets/images/onbooardingimage.png";
+
+  /// Assets for imagesSepha
+  /// assets/images/sepha.png
+  static const String imagesSepha = "assets/images/sepha.png";
+
+  /// Assets for imagesSplashimage
+  /// assets/images/splashimage.png
+  static const String imagesSplashimage = "assets/images/splashimage.png";
+
+  /// Assets for imagesZaker
+  /// assets/images/zaker.png
+  static const String imagesZaker = "assets/images/zaker.png";
+}
+
