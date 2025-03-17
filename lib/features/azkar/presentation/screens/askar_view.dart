d841a121b6e239b075cf199ddee07f428e6b8481
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-03-17 16:10:32 +0200

    add stateless

--- lib/features/azkar/presentation/screens/askar_view.dart
+++ lib/features/azkar/presentation/screens/askar_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hasna/features/azkar/presentation/widgets/azkar_view_body.dart';
 
 class AskarView extends StatelessWidget {
   const AskarView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AzkarViewBody();
   }
 }
