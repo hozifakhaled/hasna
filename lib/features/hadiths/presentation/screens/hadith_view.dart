774483056bec73d11c09213f706e052a63723e48
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-03-17 16:09:39 +0200

    add

---
+++ lib/features/hadiths/presentation/screens/hadith_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hasna/features/hadiths/presentation/widgets/hadith_view_body.dart';
+
+class HadithView extends StatelessWidget {
+  const HadithView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HadithViewBody();
+  }
+}
