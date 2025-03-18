f681a382f6c8263b4763183f6959f5c1870018d7
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-03-18 16:11:12 +0200

    change

--- lib/features/onboarding/presentation/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/widgets/onboarding_view_body.dart
@@ -33,7 +33,10 @@ class OnboardingViewBody extends StatelessWidget {
               Text('اذكر الله يذكرك، واشكره يزدك',style:  TextStyles.text20.copyWith(color: Colors.white,fontWeight: FontWeight.bold),),
           SizedBox(
             width:context.width*0.6,
-            child: CustomButton(text: 'ابدأ الان',buttonbodycolor: Colors.white,textcolor: AppColors.maincolor,onTap: (){},))
+            child: CustomButton(text: 'ابدأ الان',buttonbodycolor: Colors.white,textcolor: AppColors.maincolor,onTap: (){
+
+              GoRouter.of(context).push(Routes.hadith); 
+            },))
             ],
           ),
         ),
