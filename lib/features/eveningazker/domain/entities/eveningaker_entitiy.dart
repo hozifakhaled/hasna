bf973893b537f9c354bd920da6e378b481a58214
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-04-15 13:51:53 +0200

    evening entitiy

---
+++ lib/features/eveningazker/domain/entities/eveningaker_entitiy.dart
@@ -0,0 +1,17 @@
+class EveningakerEntitiy {
+  final String id;
+  final String description;
+  final String audioUrl;
+  final String esnadname;
+  final int count;
+  final int totalAzkar;
+
+  EveningakerEntitiy({
+    required this.id,
+    required this.description,
+    required this.audioUrl,
+    required this.esnadname,
+    required this.count,
+    required this.totalAzkar,
+  });
+}
--- lib/features/eveningazker/presentation/widgets/eveningazker_body_view.dart
+++ lib/features/eveningazker/presentation/widgets/eveningazker_body_view.dart
@@ -37,14 +37,26 @@ class _EveningazkerBodyViewState extends State<EveningazkerBodyView> {
   ];
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       controller: _pageController,
       onPageChanged: (index) {
         setState(() {
           _currentPage = index;
         });
       },
-      children: pages,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return  Zaker(
+      zaker:
+          'اللهم إني أصبحت منك في نعمة و عافية و ستر فأتمم نعمتك علي و عافيتك و سترك في الدنيا و الآخرة',
+      asnad:
+          'قال ابن السني في عمل اليوم والليلة 55 حدثني عبيد الله بن شبيب بن عبد الملك عن يزيد ابن سنان حدثنا عمرو بن الحصين حدثنا إبراهيم بن عبد الملك عن قتادة عن سعيد بن أبي الحسن عن ابن عباس رضي الله عنه قال قال رسول الله صلى الله عليه وسلم من قال إذا أصبح (اللهم إني أصبحت منك في نعمة وعافية وستر فأتمم علي نعمتك وعافيتك وسترك في الدنيا والآخرة) ثلاث مرات إذا أصبح وإذا أمسى كان حقا على الله عز وجل أن يتم عليه نعمته.وفي إسناده إبراهيم بن عبدالملكقال الحافظ ابن حجر في تهذيب التهذيب ج1:ص123 إبراهيم بن عبد الملك البصري أبو إسماعيل القناد روى عن يحيى بن أبي كثير وقتادةوعنه عبد الصمد بن عبد الوارث ويحيى بن درست ولوين وإسحاق بن أبي إسرائيل وغيرهم',
+      totalAzker: 25,
+      currentAzker: 4,
+      number: 3,
+      numberofzaker: '3 مرات ',
+    );
+      },
     );
   }
 }
