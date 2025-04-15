548d1cdd8b67045740f0961aced03ad8c15493bb
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-04-15 13:57:02 +0200

    Add evening model

--- lib/features/eveningazker/data/models/evening_model.dart
+++ lib/features/eveningazker/data/models/evening_model.dart
@@ -1,1 +1,36 @@
-class EveningModel {}
+import 'package:hasna/features/eveningazker/domain/entities/eveningaker_entitiy.dart';
+
+class EveningModel extends EveningakerEntitiy {
+  EveningModel({
+    required super.id,
+    required super.description,
+    required super.audioUrl,
+    required super.esnadname,
+    required super.count,
+    required super.totalAzkar,
+  });
+
+  factory EveningModel.fromJson(Map<String, dynamic> json) {
+    final zekr = json['zekr']; 
+
+    return EveningModel(
+      id: zekr['id'] as String,
+      description: zekr['description'] as String,
+      audioUrl: zekr['audioUrl'] as String,
+      esnadname: zekr['esnadname'] as String,
+      count: zekr['count'] as int,
+      totalAzkar: json['totalAzkar'] as int,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'description': description,
+      'audioUrl': audioUrl,
+      'esnadname': esnadname,
+      'count': count,
+      'totalAzkar': totalAzkar,
+    };
+  }
+}
