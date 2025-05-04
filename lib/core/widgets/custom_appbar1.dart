167feae5d46724793731093a7863fb3b5431389d
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-05-04 15:00:22 +0300

    add icon in hadith feature

--- lib/core/widgets/custom_appbar1.dart
+++ lib/core/widgets/custom_appbar1.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hasna/constants/images.dart';
 import 'package:hasna/core/texts_styleing/text_styles.dart';
 import 'package:hasna/core/themeing/colors.dart';
 
@@ -8,27 +7,41 @@ class CustomAppbar1 extends StatelessWidget {
   const CustomAppbar1({super.key, required this.title, this.image, this.color});
   final String title;
   final String? image;
-  final Color ? color;
+  final Color? color;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20.w),
       width: double.infinity,
       height: 70.h,
-     // color: AppColors.maincolor,
       decoration: BoxDecoration(
-         
-        color:color?? AppColors.maincolor,
+        color: color ?? AppColors.maincolor,
         borderRadius: BorderRadius.only(
           bottomLeft: Radius.circular(15.r),
           bottomRight: Radius.circular(15.r),
         ),
       ),
       child: Row(
-        mainAxisAlignment:  MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(title,style:  TextStyles.text20.copyWith(color: Colors.white,fontWeight: FontWeight.bold),),
-          Image.asset(image?? Assets.imagesIconinappbar,width: 24.w,height: 24.h,)
+          Text(
+            title,
+            style: TextStyles.text20.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.white,
+              size: 24.sp,
+            ),
+          ),
         ],
       ),
     );
